2d653bd6841b167fcf6da0c0f417c1aaef1905a6
/mythili <Mythilisrikota@gmail.com>
2021-04-07 11:31:25 +0530

    first commit

---
+++ json.js
@@ -0,0 +1,16 @@
+var data = [
+  {
+    id: 1,
+    Name: "mythili",
+    email: "mythilisrikota@gmail.com",
+    mobile: [
+      { id: 1, no: 8125370426, name: [{ id: 1, data: "mythili" }] },
+      { id: 2, no: 545425152 },
+    ],
+    images: [
+      { id: 1, img: "3453.jpg" },
+      { id: 2, img: "345.jpg" },
+    ],
+  },
+];
+console.log(data);
